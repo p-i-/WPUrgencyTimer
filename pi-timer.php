1a57f791ac08c9dffa2251e01abe693eb7058bb3
π <pi@pipad.org>
2020-06-25 13:08:52 +0700

    Tidy pi-timer.php

--- pi-timer.php
+++ pi-timer.php
@@ -17,16 +17,12 @@ class TimerPlugin
 {
     public function __construct() 
     {
-        // pi_log('Test of pi_log' );
-
         add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts_action'));
 
-        add_shortcode('pi-timer'      , array($this, 'timer_action'));
-        add_shortcode('pi-timer-small', array($this, 'timer_small_action'));
-
-        add_shortcode('ut-offer'      , array($this, 'ut_offer_action'));
-
-        //add_shortcode('pi-create-url', array($this, 'create_url_action')); // [timer] invokes this
+        add_shortcode('pi-timer'       , array($this, 'shortcode_timer'));
+        add_shortcode('pi-timer-small' , array($this, 'shortcode_timer_small'));
+        add_shortcode('ut-offer'       , array($this, 'shortcode_ut_offer'));
+        add_shortcode('ut-flip'        , array($this, 'shortcode_ut_flip'));
     }
 
     function enqueue_scripts_action()
@@ -44,27 +40,33 @@ class TimerPlugin
           );
     }
 
-
-    function timer_action($atts) {
-        // $content = <<<EOS
+    function shortcode_timer($atts) {
          ob_start();
-?>
+        ?>
         <span class='urgency-timer ut-large'>
-          <span class='ut-numbers ut-hh'>03</span>  <span class=ut-letters>h</span>
-          <span class='ut-numbers ut-mm'>02</span>  <span class=ut-letters>m</span>
-          <span class='ut-numbers ut-ss'>01</span>  <span class=ut-letters>s</span>
+            <span class='ut-numbers ut-hh'>03</span>  <span class=ut-letters>h</span>
+            <span class='ut-numbers ut-mm'>02</span>  <span class=ut-letters>m</span>
+            <span class='ut-numbers ut-ss'>01</span>  <span class=ut-letters>s</span>
         </span>
-
-<?php
+        <?php
         return ob_get_clean();
-        // EOS;
-        // return $content;
     }
 
+    function shortcode_timer_small($atts) {
+        ob_start();
+        ?>
+        <span class='urgency-timer ut-small'>
+            <span class=ut-hh>03</span>  <span class=ut-letters>:</span>
+            <span class=ut-mm>02</span>  <span class=ut-letters>:</span>
+            <span class=ut-ss>01</span>
+        </span>
+        <?php
+        return ob_get_clean();
+    }
 
     // $atts is e.g. `from='$99' to='$1'`
     // Display correct item depending on timer visible-status 
-    function ut_offer_action($atts) {
+    function shortcode_ut_offer($atts) {
         $fullprice = $atts['from'];
         $discount = $atts['to'];
 
@@ -74,63 +76,35 @@ class TimerPlugin
         // If timer is visible, 'ut-priceslash-parent' and 'ut-reduced-price' are hidden
         //     else, 'ut-no-discount' is visible.
         ?>
-<span class='ut-no-discount'><?php echo $fullprice;?></span>
-<span class='ut-priceslash-parent'>
-    <span class='ut-priceslash-text'><?php echo $fullprice;?></span>
-    <img class='ut-priceslash-strike' src='<?php echo $image_url;?>' />
-</span>
+        <span class='ut-no-discount'><?php echo $fullprice;?></span>
+        <span class='ut-priceslash-parent'>
+            <span class='ut-priceslash-text'><?php echo $fullprice;?></span>
+            <img class='ut-priceslash-strike' src='<?php echo $image_url;?>' />
+        </span>
 
-<span class='ut-reduced-price'><b>
-        <?php echo $discount;?>
-</b>
-</span>
+        <span class='ut-reduced-price'>
+            <b><?php echo $discount;?></b>
+        </span>
         <?php
         return ob_get_clean();
     }
 
-//         echo "<span class='ut-from'>$from</span>";
-//         echo "<span class='ut-price-strike'>$from</span>";
-//         echo "<span class='ut-to'>$to</span>";
+    function shortcode_ut_flip($atts) {
+    //     $text_hidden = $atts['hidden'];
+    //     $text_visible = $atts['visible'];
 
-//         $content = <<<EOS
-//             <span>
-//             <script>
-//                 window.addEventListener('DOMContentLoaded', () => {
-//                     // add on-change handler
-//                     window.piTimer.registerOnUpdateHook(
-//                         (timerVisible) => {
-//
-//                         }
-//                     );
-//                 }
-//             </script>
-//             EOS;
-//     }
-
-
-    function timer_small_action($atts) {
-        $content = <<<EOS
-        <span class='urgency-timer ut-small'>
-          <span class='ut-numbers ut-hh'>03</span>  <span class=ut-letters>:</span>
-          <span class='ut-numbers ut-mm'>02</span>  <span class=ut-letters>:</span>
-          <span class='ut-numbers ut-ss'>01</span>
-        </span>
-        EOS;
-        return $content;
+    //     ob_start();
+    //     ? >
+    //     <script>
+    //         window.addEventListener('DOMContentLoaded', () => {
+    //             window.piTimer.registerOnUpdateHook( is_vis => {
+    //                 document.write( "x" );
+    //             } );
+    //         }
+    //     </script>
+    //     <? php
+    //     return ob_get_clean();
     }
-
-//     function create_url_action($atts) {
-//         $content = <<<EOS
-// <a href='#' class='my_href'>my-link-text</a>
-// EOS;
-//         return $content;
-//     }
-
 }
-/* $wpPlugin = */ new TimerPlugin();
 
-// <div class=clockdiv>
-//   <span class=number hours>  </span><span class=smalltext>h</span>
-//   <span class=number minutes></span><span class=smalltext>m</span>
-//   <span class=number seconds></span><span class=smalltext>s</span>
-// </div>
+/* $wpPlugin = */ new TimerPlugin();
